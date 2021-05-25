167c2cb3a35832c1ee5f7b62bea74d3a9a208e87
ryryqtqt <ryescalera@gmail.com>
2021-05-25 16:43:18 +0800

    AdminAccountSeeder

---
+++ database/seeders/AdminAccountSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use DB, Carbon;
+
+class AdminAccountSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('user')->insert([
+            'name' => 'Super Admin',
+            'username' => 'admin',
+            'password' => bcrypt('1234'),
+
+        ]);
+    }
+}
